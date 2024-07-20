c74edf69b5728bb1502e8bf72cdf02c38469742c
Daksh Purohit <dakshgrows@gmail.com>
2024-07-20 22:31:13 +0530

    moving toward dashboard

--- server/routes/book.js
+++ server/routes/book.js
@@ -30,4 +30,33 @@ router.get('/books', async(req, res) => {
     }
 })
 
+router.get('/book/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const book = await Book.findById({_id: id})
+        return res.json(book)
+    } catch(err) {
+        return res.json(err)
+    }
+})
+router.put('/book/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const book = await Book.findByIdAndUpdate({_id: id}, req.body)
+        return res.json({updated: true, book})
+    } catch(err) {
+        return res.json(err)
+    }
+})
+
+router.delete('/book/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const book = await Book.findByIdAndDelete({_id: id})
+        return res.json({deleted: true, book})
+    } catch(err) {
+        return res.json(err)
+    }
+})
+
 export { router as bookRouter }
