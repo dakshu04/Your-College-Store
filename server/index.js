7ea8bf6209f5f0fc5dfbd12c90c8d008db97b852
Daksh Purohit <dakshgrows@gmail.com>
2024-07-20 23:28:42 +0530

    all set

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import { AdminRouter } from './routes/auth.js';
 import './db.js';
 import { studentRouter } from './routes/student.js';
 import { bookRouter } from './routes/book.js';
+import { Book } from './models/Book.js'
+import { Student } from './models/Student.js'
+import { Admin } from './models/Admin.js'
+import { ok } from 'assert';
 
 // Load environment variables as early as possible
 dotenv.config();
@@ -25,11 +29,24 @@ app.use('/auth', AdminRouter);
 app.use('/student', studentRouter);
 app.use('/book', bookRouter);   
 
+
 // Basic error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
+app.get('/dashboard', async(req, res) => {
+    try {
+        const student = await Student.countDocuments()
+        const admin = await Admin.countDocuments()
+        const book = await Book.countDocuments()
+        return res.json({ok: true, student, book, admin})
+    } catch(err) {
+        return res.json(err)
+    }
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
